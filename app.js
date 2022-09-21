1402001ac73d4491015495358efa142ee85f1183
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-09-21 21:04:51 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.listen(8080);
